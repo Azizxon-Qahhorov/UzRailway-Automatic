03bfaf62a5236e6c3c43a374b3124b5bea718c82
Azizxon Qahhorov <113577721+220536cs14@users.noreply.github.com>
2026-04-19 15:55:41 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ TO_STATION = "Toshkent"
 DAY = "25"                    # sana (kun)
 MAX_COST = 166000             # so'm
 
-TELEGRAM_BOT_TOKEN = "8583653173:AAGaIc7oXNMLlsVM7C6L06dmrhd1DGl8j-Y"
-CHAT_ID = "1787742278"
+TELEGRAM_BOT_TOKEN = "BOT_TOKEN"
+CHAT_ID = "Telegram ID"
 DAY = "25"  # kun
 MONTH_NAME = "MART 2026"  # oy va yil calendardagi ko‘rinishi bilan
 
-CHROMEDRIVER_PATH = r"D:\Test\Python\Testbot\InstaTopBot\chromedriver-win32\chromedriver.exe"
+CHROMEDRIVER_PATH = r"PASTE_CHROMEDRIVER_PATH_ON_YOUR_DISC"
 URL = "https://eticket.railway.uz/"
 # ====================
