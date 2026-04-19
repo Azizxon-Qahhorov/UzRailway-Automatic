c8314d32268cd4b6f5ee12faad42d257a15ada07
Azizxon Qahhorov <113577721+220536cs14@users.noreply.github.com>
2026-04-19 16:05:01 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 ﻿# ====== CONFIG ======
 CHECK_INTERVAL = 5          # 5 daqiqa
-FROM_STATION = "Qo'qon"
-TO_STATION = "Toshkent"
+FROM_STATION = "Qo'qon"     # Jo'namoqchi bo'lgan hududni kiriting
+TO_STATION = "Toshkent"     # Bormoqchi bo'lgan manzilni kiriting
 DAY = "25"                    # sana (kun)
 MAX_COST = 166000             # so'm, bu qator uncha muhim emas chunki kodni logikasi har qanday 
                               #turdagi narxlarni topishga qaratilgan tarzda yozilgan edi Yangi Yil arafasida
 
 TELEGRAM_BOT_TOKEN = "BOT_TOKEN"
-CHAT_ID = "Telegram ID"
+CHAT_ID = "Telegram_USER_ID"
 DAY = "25"  # kun
 MONTH_NAME = "MART 2026"  # oy va yil calendardagi ko‘rinishi bilan
 
-CHROMEDRIVER_PATH = r"PASTE_CHROMEDRIVER_PATH_ON_YOUR_DISC"
-URL = "https://eticket.railway.uz/"
+CHROMEDRIVER_PATH = r"PASTE_CHROMEDRIVER_PATH_ON_YOUR_DISC" # example 
+                             #  "D:\Test\Python\Testbot\InstaTopBot\chromedriver-win32\chromedriver.exe"
+URL = "https://eticket.railway.uz/"   # agarda sayt domeni o'zgarmasa, bunga tegmang
 # ====================
